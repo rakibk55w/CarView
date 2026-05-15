3ab750a29a81ac44384f2d04a362610012995c90
rakibk55w <rakibk55w@gmail.com>
2026-05-15 22:11:24 +0600

    Setup database connection

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const app = require("./app");
+const pool = require("./config/db");
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+pool
+  .connect()
+  .then(() => {
+    console.log("Database connected");
+
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}/api/`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed");
+    console.log(err);
+  });
