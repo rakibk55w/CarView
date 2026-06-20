07a8f538801050b3d3fc7cc448d9cb0569ac5f00
rakibk55w <rakibk55w@gmail.com>
2026-06-20 22:00:47 +0600

    Added auth check in logout route

--- routes/logout_route.js
+++ routes/logout_route.js
@@ -1,7 +1,8 @@
 const logoutRouter = require("express").Router();
 const logoutLimiter = require("../middlewares/limiters/logout_limiter");
+const authChecker = require("../middlewares/auth_checker");
 const logoutController = require("../controllers/logout_controller");
 
-logoutRouter.post("/auth/logout", logoutLimiter, logoutController);
+logoutRouter.post("/auth/logout", logoutLimiter, authChecker, logoutController);
 
 module.exports = logoutRouter;
