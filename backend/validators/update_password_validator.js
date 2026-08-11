dee1cb10bd5664c1e16ef04bc411f4a142b4e1e8
rakibk55w <rakibk55w@gmail.com>
2026-08-12 01:38:40 +0600

    fixed an import issue.

--- backend/validators/update_password_validator.js
+++ backend/validators/update_password_validator.js
@@ -1,4 +1,4 @@
-const updatePasswordSchema = require("../schemas/update_Password_schema");
+const updatePasswordSchema = require("../schemas/update_password_schema");
 const runValidator = require("../validators/run_validator");
 
 const updatePasswordValidator = runValidator(updatePasswordSchema);
